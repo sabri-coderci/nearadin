3a95c9f424ba098fa678dcb8c75affb74deaab1e
Sabri <talaysabri@gmail.com>
2026-08-09 00:19:47 +0300

    Integrate Admatic AUTO ads into fetch_news.py
    Added Admatic AUTO ads integration to the HTML structure.

--- fetch_news.py
+++ fetch_news.py
@@ -98,6 +98,11 @@ def fetch_and_generate():
     </style>
 </head>
 <body>
+
+<!-- Admatic AUTO ads START -->
+<ins data-publisher="adm-pub-342021502" data-ad-network="6938571fadda546eb28ca492"   class="adm-ads-area"></ins>
+<script type="text/javascript" src="https://static.cdn.admatic.com.tr/showad/showad.min.js"></script>
+<!-- Admatic AUTO ads END -->
     <header>nearadin.net - SON DAKİKA</header>
     <div class="container">
         <div class="info-box">
