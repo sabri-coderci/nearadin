9f3de5837d37d3908da3cb298e2b556d6751dc50
Sabri <talaysabri@gmail.com>
2026-09-09 11:47:39 +0300

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -331,7 +331,7 @@ def fetch_and_generate():
         "spor": {"name": "Spor", "url": "https://news.google.com/rss/headlines/section/topic/SPORTS?hl=tr&gl=TR&ceid=TR:tr"},
         "saglik": {"name": "Sağlık", "url": "https://news.google.com/rss/headlines/section/topic/HEALTH?hl=tr&gl=TR&ceid=TR:tr"},
         "bilim": {"name": "Bilim", "url": "https://news.google.com/rss/headlines/section/topic/SCIENCE?hl=tr&gl=TR&ceid=TR:tr"},
-        "bitcoin": {"name": "Bitcoin", "url": "https://news.google.com/rss/headlines/section/topic/BITCOIN?hl=tr&gl=TR&ceid=TR:tr"}
+        "bitcoin": {"name": "Bitcoin", "url": "https://news.google.com/rss/headlines/section/topic/BTC?hl=tr&gl=TR&ceid=TR:tr"}
 
     }
 
